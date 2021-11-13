e8276f9ad386fa2c89e346af1102b4b0a2fdc029
Manakal <dp.manakal82@gmail.com>
2021-11-13 19:51:06 +0530

    port changed

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 require('./routes/dialogFlowRoutes')(app);
 
-app.listen(5000);
+app.listen(process.env.PORT||5000);
 
 // export GOOGLE_APPLICATION_CREDENTIALS="C:\Users\dpman\Downloads\q-a-phem-973539eb86f5.json"
 //$env:GOOGLE_APPLICATION_CREDENTIALS="C:\\Users\\dpman\\Downloads\\q-a-phem-973539eb86f5.json"
