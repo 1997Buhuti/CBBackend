0454a7b57af4ef8e92369c65bfb341ea20d4e908
Manakal <dp.manakal82@gmail.com>
2022-06-11 13:31:49 +0530

    added google credentials key file to proc files and environment variables

--- cotrollers/controller.js
+++ cotrollers/controller.js
@@ -5,7 +5,7 @@ const { format } = require("util");
 const { Storage } = require("@google-cloud/storage");
 
 // Instantiate a storage client with credentials
-const storage = new Storage({ keyFilename: "google-cloud-key.json.json" });
+const storage = new Storage({ keyFilename: process.env.GOOGLE_APPLICATION_CREDENTIALS });
 const bucket = storage.bucket("chatbot_knowledgebases");
 
 const upload = async (req, res) => {
