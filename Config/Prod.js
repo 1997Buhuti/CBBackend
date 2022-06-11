ef3c96db8339d4639890694bd89edf5051b9b2a7
Manakal <dp.manakal82@gmail.com>
2022-06-11 10:45:54 +0530

    added proc file and DB config files

--- Config/Prod.js
+++ Config/Prod.js
@@ -3,5 +3,6 @@ module.exports = {
     dialogflowSessionId: process.env.DIALOGFLOW_SESSION_ID,
     dialogflowSessionLanguageCode: process.env.DIALOGFLOW_LANGUAGE_CODE,
     googleClientEmail: process.env.GOOGLE_CLIENT_EMAIL,
-    googlePrivateKey: JSON.parse(process.env.GOOGLE_Private_Key)
+    googlePrivateKey: JSON.parse(process.env.GOOGLE_Private_Key),
+    mongoURI:process.env.MONGODB_URI
 }
